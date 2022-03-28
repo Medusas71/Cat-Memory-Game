44df7ad182c7dde8985af47c479fd3ed99636361
Medusas71 <medusas71@icloud.com>
2022-03-28 04:36:21 +0000

    Added functionality to compare cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-
   // Array of Cat Cards
   let cards = ["cat1", "cat2", "cat3", "cat4", "cat5", "cat6"];
 
@@ -42,13 +41,53 @@
       console.log(i + " " + cardArray[i][0]);
   };
 
+
     // borrowed from https://stackoverflow.com/questions/65485778/javascript-how-can-i-create-multiple-flip-cards-that-flip-with-an-onclick
 
-    // Flip Card
+    // Flip First Card
+    // On click, turn over first card
+    // Replace picture of back of card to the front
+    // Add an Event Listener
+    
 
+    let firstCard, secondCard;
+   
     for (let firstCard of document.querySelectorAll(".card")) {
         firstCard.addEventListener("click", () => {
             firstCard.classList.toggle("flipped");
             console.log("flipped");
         });
     };
+
+    // https://www.youtube.com/watch?v=ZniVgo8U7ek 
+    // function to compare cards
+
+    if (firstCard.dataset.id === secondCard.dataset.id) {
+        firstCard.removeEventListener("click", "flipped");
+        secondCard.removeEventListener("click", "flipped");
+    } else {
+        setTimeout(() => {
+        firstCard.classList.remove("flipped");
+        secondCard.classList.remove("flipped");
+    }, 1500);
+}
+
+
+
+    // On click of second card
+    // A. Turn Over
+    // B. Compare with the first card
+    // C. If same, keep both cards flipped
+    // D. If not, flip cards over to back image
+
+    
+    // function compareCards() {
+    //     if (cardArray[i][0] === cardArray[i][0]) {
+    //         stayFlipped.removeEventListener("click");
+    //     } else {
+    //         cardArray.classList.toggle("flipped");
+    //     };
+    // }
+
+
+
